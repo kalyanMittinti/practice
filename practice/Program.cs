9e7f36dd88156fd8029f02b272cdf7b193bda45a
Kalyan M <kalyan.m@aunoasolutions.com>
2021-08-27 19:28:51 +0530

    Add project files.

---
+++ practice/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+namespace practice
+{
+    class Program
+    {
+      public static void Main()
+        {
+            Console.WriteLine("enter your eno");
+            string eno = Console.ReadLine();
+           
+     
+            Console.WriteLine("enter your ename");
+            string ename = Console.ReadLine();
+
+            Console.WriteLine("enter your salary");
+            string salary = Console.ReadLine();
+
+            Console.WriteLine("enter your phonenumber");
+            string phonenumber = Console.ReadLine();
+
+            
+            Console.WriteLine("enter your city");
+            string city = Console.ReadLine();
+
+        
+            
+
+            Console.WriteLine("hello"+ eno+ename+salary+phonenumber+city);
+
+            Program.m1();
+
+        }
+        public static void m1(database d )
+        {
+            
+            SqlConnection con = new SqlConnection(@"Data Source=KALYAN\SQLEXPRESS;Initial Catalog=csharp;User ID=sa;Password=abc");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("insert into tab values('" + d.eno + "','" + d.ename + "','" + d.salary + "','" + d.phonenumber + "','" + d.city + "')", con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+
+
+
+
+    }
+}
---
+++ practice/database.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice
+{
+    class database
+    {
+        public string eno { get; set; }
+
+        public string ename { get; set; }
+        public string salary { get; set; }
+
+        public string phonenumber { get; set; }
+
+        public string city { get; set; }
+
+    }
+}
